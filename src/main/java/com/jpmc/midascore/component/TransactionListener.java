d926b82f4c66346e046ae6ef38e9f1b16d26512b
Hardeep Singh <hs0774@gmail.com>
2026-06-30 00:24:56 -0400

    Task 3 complete

--- src/main/java/com/jpmc/midascore/component/TransactionListener.java
+++ src/main/java/com/jpmc/midascore/component/TransactionListener.java
@@ -1,13 +1,43 @@
 package com.jpmc.midascore.component;
 
+import com.jpmc.midascore.entity.TransactionRecord;
+import com.jpmc.midascore.entity.UserRecord;
 import com.jpmc.midascore.foundation.Transaction;
+import com.jpmc.midascore.repository.TransactionRepository;
+import com.jpmc.midascore.repository.UserRepository;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
 @Component
 public class TransactionListener {
+    private final UserRepository userRepository;
+    private final TransactionRepository transactionRepository;
+    public TransactionListener(UserRepository userRepository, TransactionRepository transactionRepository) {
+        this.userRepository = userRepository;
+        this.transactionRepository = transactionRepository;
+    }
+
     @KafkaListener(topics = "${general.kafka-topic}")
     public void listen(Transaction transaction) {
-        System.out.println("Received Transaction: " + transactions);
+       UserRecord sender = userRepository.findById(transaction.getSenderId());
+       UserRecord receiver = userRepository.findById(transaction.getRecipientId());
+       float amount = transaction.getAmount();
+       if(sender == null) {
+           return;
+       }
+       if(receiver == null) {
+           return;
+       }
+       if (amount > sender.getBalance()) {
+           return;
+       }
+       sender.setBalance(sender.getBalance() - amount);
+       receiver.setBalance(receiver.getBalance() + amount);
+
+       userRepository.save(sender);
+       userRepository.save(receiver);
+
+        TransactionRecord record = new TransactionRecord(sender,receiver,amount);
+        transactionRepository.save(record);
     }
 }
